1ced45d262a706c77ce0820690454027688efba7
Oracle Public Cloud User <opc@pseudos.subnet05271522.vcn05271522.oraclevcn.com>
2023-04-01 09:31:45 +0000

    fix non optional pivot move flag

--- sim/dex-moves.ts
+++ sim/dex-moves.ts
@@ -51,7 +51,7 @@ interface MoveFlags {
 	nonsky?: 1; // Prevented from being executed or selected in a Sky Battle.
 	noparentalbond?: 1; // Cannot be made to hit twice via Parental Bond.
 	nosleeptalk?: 1; // Cannot be selected by Sleep Talk.
-	pivot: 1; // Fluffy bond grants an immunity to the move.
+	pivot?: 1; // Fluffy bond grants an immunity to the move.
 	pledgecombo?: 1; // Gems will not activate. Cannot be redirected by Storm Drain / Lightning Rod.
 	powder?: 1; // Has no effect on Pokemon which are Grass-type, have the Ability Overcoat, or hold Safety Goggles.
 	protect?: 1; // Blocked by Detect, Protect, Spiky Shield, and if not a Status move, King's Shield.
